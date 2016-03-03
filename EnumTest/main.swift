3ff2e4ec4ca4b2519ab9d4d5745c1e328d83e447
Alexander von Below <Alex@vonBelow.Com>
2016-03-03 11:34:00 +0100

    Initial Commit

---
+++ EnumTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EnumTest
+//
+//  Created by Alexander v. Below on 03.03.16.
+//  Copyright © 2016 Alexander von Below. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
